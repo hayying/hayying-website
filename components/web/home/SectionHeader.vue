5a6ebf1bb05afd7f1445b921027b1a4e54241fb2
yali <tangozyusufali@gmail.com>
2024-05-10 15:35:14 +0300

    feat: section header

---
+++ components/web/home/SectionHeader.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+defineProps({
+  title: String,
+  description: String,
+});
+</script>
+
+<template>
+  <UContainer class="relative">
+    <UDivider class="w-32 m-auto" :ui="{ border: { base: 'border-2' } }" />
+    <UDivider
+      class="w-16 absolute -top-[.01rem] left-1/2 transform -translate-x-1/2"
+      :ui="{ border: { base: 'border-violet-500 border-2 ' } }"
+    />
+    <h1 class="text-center text-2xl font-bold text-gray-800 mt-5">
+      {{ title }}
+    </h1>
+    <p class="text-center text-gray-500 mt-2">{{ description }}</p>
+  </UContainer>
+</template>
