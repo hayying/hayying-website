17ae5eb4f0d651b887163bafc27b67b24922a530
yali <tangozyusufali@gmail.com>
2024-05-10 21:21:15 +0300

    feat: online counseling

--- components/web/PageHeader.vue
+++ components/web/PageHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-const { img } = defineProps<{
+defineProps<{
   title: string;
   img: string;
 }>();
@@ -7,8 +7,9 @@ const { img } = defineProps<{
 
 <template>
   <div
-    :class="`text-white bg-[url('${img}')] bg-cover h-[20rem] md:h-[28rem] bg-center grid place-items-center relative`"
+    :class="`text-white h-[20rem] md:h-[28rem] bg-center grid place-items-center relative`"
   >
+    <img :src="img" class="w-full h-full object-cover absolute" />
     <div class="bg-indigo-800 w-full h-full opacity-50 absolute" />
     <div
       class="z-10 grid place-items-center gap-5"
---
+++ pages/online-counseling.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Online Danışmanlık",
+  description: "Hayying ile online danışmanlık hizmeti alın",
+});
+
+const { data }: { data: any } = await useApiFetch("/online-consulting");
+</script>
+<template>
+  <WebPageHeader img="/online-counseling.jpg" title="ONLİNE DANIŞMANLIK" />
+  <UContainer class="mt-32">
+    <MDC :value="data.data.attributes.content" />
+  </UContainer>
+</template>
