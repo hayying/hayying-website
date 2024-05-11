31cde5cab26fdbc37995afd82eb4e6d4a0afbf50
yali <tangozyusufali@gmail.com>
2024-05-11 21:41:12 +0300

    feat: courses

---
+++ pages/courses/[slug].vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">
+const { currentRoute } = useRouter();
+const { slug } = currentRoute.value.params;
+
+const { data }: { data: any } = await useApiFetch(
+  `/courses?populate=*&filters[slug][$eq]=${slug}`
+);
+
+const { isAuthenticated } = useUserStore();
+const config = useRuntimeConfig();
+</script>
+
+<template>
+  <WebPageHeader
+    :title="data.data[0].attributes.title"
+    img="/policy-banner.jpg"
+  />
+  <UContainer class="my-32 flex flex-wrap gap-10 md:flex-nowrap justify-center">
+    <div class="w-full">
+      <h1 class="text-2xl mb-3">
+        {{ data.data[0].attributes.title }}
+      </h1>
+      <MDC :value="data.data[0].attributes.details" class="break-all" />
+    </div>
+    <UCard :ui="{ body: { base: 'grid gap-3 ', background: 'bg-gray-100' } }">
+      <img
+        alt="blog"
+        class="md:max-w-md md:w-auto w-full object-cover"
+        :src="config.public.apiImgUrl + data.data[0].attributes.thumbnail.url"
+      />
+      <div class="flex items-end justify-between mt-3">
+        <span class="text-3xl" v-if="data.data[0].attributes.price !== 0"
+          >{{ data.data[0].attributes.price }}₺</span
+        >
+
+        <span
+          class="text-gray-500 text-lg ml-auto"
+          :class="{
+            'text-green-500': data.data[0].attributes.type === 'online',
+            'text-red-500': data.data[0].attributes.type === 'offline',
+          }"
+        >
+          {{ data.data[0].attributes.type.toUpperCase() }}
+        </span>
+      </div>
+      <UButton
+        v-if="data.data[0].attributes.price !== 0"
+        label="Sepete Ekle"
+        size="xl"
+        block
+      />
+      <UButton label="İletişime Geç" to="/contact" block size="xl" v-else />
+
+      <p class="text-gray-500">
+        {{ data.data[0].attributes.description }}
+      </p>
+    </UCard>
+  </UContainer>
+</template>
---
+++ pages/courses/index.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+const { data }: { data: any } = await useApiFetch("/courses?populate=*");
+const config = useRuntimeConfig();
+const type = ref("all");
+
+const courses = computed(() => {
+  return data.value.data.filter((item: any) => {
+    if (type.value === "all") return true;
+    return item.attributes.type === type.value;
+  });
+});
+
+function setType(value: string) {
+  type.value = value;
+}
+</script>
+
+<template>
+  <WebPageHeader title="KURSLAR" img="/online-counseling.jpg" />
+  <div class="bg-primary-500 p-3">
+    <UContainer>
+      <UButtonGroup size="sm" orientation="horizontal">
+        <UButton
+          label="Hepsi"
+          @click="setType('all')"
+          :color="type === 'all' ? 'blue' : 'gray'"
+        />
+        <UButton
+          label="Online"
+          @click="setType('online')"
+          :color="type === 'online' ? 'blue' : 'gray'"
+        />
+        <UButton
+          label="Offline"
+          @click="setType('offline')"
+          :color="type === 'offline' ? 'blue' : 'gray'"
+        />
+      </UButtonGroup>
+    </UContainer>
+  </div>
+  <UContainer class="my-32">
+    <div class="grid md:grid-cols-3 gap-10">
+      <NuxtLink
+        class="shadow-xl transition-all"
+        v-for="item in courses"
+        :to="`/courses/${item.attributes.slug}`"
+      >
+        <div class="relative group overflow-hidden">
+          <img
+            :src="config.public.apiImgUrl + item.attributes.thumbnail.url"
+            class="h-[17rem] w-full group-hover:scale-110 transition-all"
+            draggable="false"
+          />
+          <div
+            class="absolute bottom-0 left-5 bg-black opacity-70 text-white p-2 py-1"
+          >
+            {{ item.attributes.price }}₺
+          </div>
+          <UButton
+            class="absolute hidden group-hover:block left-[50%] bottom-5 transform -translate-x-1/2 top-[50%] -translate-y-1/2 h-max opacity-70 rounded-full"
+            label="Detaylar"
+            color="gray"
+          />
+        </div>
+        <div class="p-5">
+          <span
+            class="text-gray-500 text-sm mb-2"
+            :class="{
+              'text-green-500': item.attributes.type === 'online',
+              'text-red-500': item.attributes.type === 'offline',
+            }"
+          >
+            {{ item.attributes.type.toUpperCase() }}
+          </span>
+          <h1 class="text-lg font-bold text-gray-800">
+            {{ item.attributes.title }}
+          </h1>
+          <p class="text-gray-500">{{ item.attributes.description }}</p>
+        </div>
+        <UDivider />
+      </NuxtLink>
+    </div>
+  </UContainer>
+</template>
