b720c391d6aea5739221c10c2e8600d67567a6ca
yali <tangozyusufali@gmail.com>
2024-05-11 17:36:11 +0300

    feat: error page

---
+++ error.vue
@@ -0,0 +1,10 @@
+<template>
+  <NuxtLayout>
+    <div
+      class="min-h-screen bg-[url('/404.svg')] bg-primary-900 flex flex-col gap-10 place-items-center justify-center text-white"
+    >
+      <h1 class="text-7xl md:text-9xl font-extrabold">404</h1>
+      <UButton label="Anasayfaya Dön" to="/" color="black" size="xl" />
+    </div>
+  </NuxtLayout>
+</template>
