b353702744f92586f41d8205610f2cfccfc69f78
yali <tangozyusufali@gmail.com>
2024-05-13 23:52:15 +0300

    style: update socials zindex

--- components/web/Socials.vue
+++ components/web/Socials.vue
@@ -3,7 +3,7 @@ const socials = useState<any>("socials");
 </script>
 
 <template>
-  <div class="fixed bottom-32 right-[3.2rem] grid gap-3">
+  <div class="fixed bottom-32 right-[3.2rem] grid gap-3 z-50">
     <NuxtLink v-for="social in socials" :key="social.id" :to="social.href">
       <UButton :icon="social.icon" size="xl" />
     </NuxtLink>
