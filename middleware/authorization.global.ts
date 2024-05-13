821ef755151addd37fc514ed4a2d104f8e0c4812
yali <tangozyusufali@gmail.com>
2024-05-13 23:31:21 +0300

    feat: authorization middleware

---
+++ middleware/authorization.global.ts
@@ -0,0 +1,18 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  const { isAuthenticated } = useUserStore();
+  const needAuth = [
+    "/dashboard",
+    "/dashboard/courses",
+    "/dashboard/events",
+    "/dashboard/orders",
+    "/dashboard/settings",
+    "/online-form",
+  ];
+  const alreadyAuth = ["/login", "/register"];
+  if (needAuth.includes(to.path) && !isAuthenticated) {
+    return navigateTo("/login");
+  }
+  if (alreadyAuth.includes(to.path) && isAuthenticated) {
+    return navigateTo("/");
+  }
+});
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,5 @@
+<template></template>
+
+<script setup lang="ts">
+navigateTo("/dashboard/courses");
+</script>
