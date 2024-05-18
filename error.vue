0ca8929ac36ce3c6b3f4e65f2fe8f7ccada080d7
yali <tangozyusufali@gmail.com>
2024-05-18 18:42:10 +0300

    feat: add status and message to error component

--- error.vue
+++ error.vue
@@ -1,9 +1,21 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
 <template>
   <NuxtLayout>
     <div
       class="min-h-screen bg-[url('/404.svg')] bg-primary-900 flex flex-col gap-10 place-items-center justify-center text-white"
     >
-      <h1 class="text-7xl md:text-9xl font-extrabold">404</h1>
+      <h1 class="text-7xl md:text-9xl font-extrabold">
+        {{ error?.statusCode }}
+      </h1>
+      <p class="text-2xl md:text-3xl font-semibold">
+        {{ error?.statusMessage }}
+      </p>
       <UButton label="Anasayfaya Dön" to="/" color="black" size="xl" />
     </div>
   </NuxtLayout>
