5708ed119181d8c050f0cf04c9bc3f3de9551831
yali <tangozyusufali@gmail.com>
2024-05-27 20:17:52 +0300

    style(header): update height

--- components/web/Header.vue
+++ components/web/Header.vue
@@ -3,7 +3,7 @@ const socials = useState<any>("socials");
 </script>
 <template>
   <header
-    class="bg-orange-500 fixed top-0 z-50 left-0 p-2 w-full flex justify-end gap-3 pr-6"
+    class="bg-orange-500 h-[36px] fixed top-0 z-50 left-0 p-2 w-full flex justify-end gap-3 pr-6"
   >
     <UButton
       v-for="social in socials"
