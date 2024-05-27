40f4a3c3114cf99d43798925fd5c6ea1d6836cfc
yali <tangozyusufali@gmail.com>
2024-05-27 20:51:47 +0300

    Revert "feat(home): update heroheader and search"
    This reverts commit 0e73339141dde9c99758e974bf5f2f601af466be.

---
+++ components/web/home/Features.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+const features = [
+  {
+    title: "Kurslar",
+    to: "/courses",
+    image: "/feature-1.png",
+    color: "green",
+  },
+  {
+    title: "Etkinlikler",
+    to: "/events",
+    image: "/feature-2.png",
+    color: "orange",
+  },
+  {
+    title: "Blog",
+    to: "/blog",
+    image: "/feature-3.png",
+    color: "blue",
+  },
+  {
+    title: "Online Form",
+    to: "/online-form",
+    image: "/feature-4.png",
+    color: "red",
+  },
+];
+</script>
+
+<template>
+  <UContainer :ui="{ constrained: 'max-w-[100rem]' }">
+    <section class="flex flex-wrap justify-center gap-5 relative bottom-24">
+      <section
+        :class="`bg-${color}-500 p-10 flex flex-col items-center rounded-md px-20`"
+        v-for="{ color, image, title, to } in features"
+        :key="title"
+      >
+        <img :src="image" alt="feature" class="w-32 h-32" />
+        <h1 class="text-2xl text-white">
+          {{ title }}
+        </h1>
+        <UButton
+          class="mt-5 px-2.5 opacity-30 text-gray-500 rounded-full"
+          icon="icon-park-outline:right"
+          variant="soft"
+          color="gray"
+          size="xl"
+          :to="to"
+        />
+      </section>
+    </section>
+  </UContainer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <WebHomeHeroHeader />
-  <WebHomeSearch />
+  <WebHomeFeatures />
   <WebHomeCourses />
   <WebHomeEvents />
   <WebHomeContact />
