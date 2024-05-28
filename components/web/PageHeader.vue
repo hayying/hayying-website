be04dc662b2221116f041899341c62d65efc8c03
yali <tangozyusufali@gmail.com>
2024-05-28 13:30:33 +0300

    style: update page header

--- components/web/PageHeader.vue
+++ components/web/PageHeader.vue
@@ -3,24 +3,31 @@ defineProps<{
   title: string;
   img: string;
 }>();
+const links = useBreadcrumbItems();
 </script>
 
 <template>
   <section
-    class="text-white h-[20rem] md:h-[28rem] mt-20 bg-center grid place-items-center relative border-b-[5px] border-primary-500"
+    class="text-white h-[20rem] md:h-[28rem] mt-20 bg-center relative ring-2 ring-gray-400"
   >
     <img :src="img" class="w-full h-full object-cover absolute" />
-    <section class="bg-primary-800 w-full h-full opacity-50 absolute" />
     <section
-      class="z-10 grid place-items-center gap-5"
-      v-motion
-      :initial="{ opacity: 0, y: 100 }"
-      :enter="{ opacity: 1, y: 0, transition: { duration: 1500 } }"
-    >
-      <section class="w-20 rounded-full h-1 bg-primary-500" />
-      <h1 class="text-xl max-w-7xl md:text-4xl font-bold text-center">
+      class="bg-gradient-to-r from-black to-transparent w-full h-full opacity-50 absolute"
+    />
+    <UContainer class="pt-20 md:pt-44">
+      <UBreadcrumb
+        :ui="{
+          li: 'text-white',
+        }"
+        :links="links"
+      >
+        <template #divider>
+          <UIcon name="mingcute:right-line" />
+        </template>
+      </UBreadcrumb>
+      <h1 class="text-2xl mt-2 absolute">
         {{ title }}
       </h1>
-    </section>
+    </UContainer>
   </section>
 </template>
