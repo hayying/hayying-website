24a1a09ca3d9190062635736101dc1536070fc81
yali <tangozyusufali@gmail.com>
2024-05-29 12:00:02 +0300

    style(navbar): center links

--- components/web/Navbar.vue
+++ components/web/Navbar.vue
@@ -48,24 +48,17 @@ const isOpen = ref(false);
   <nav
     class="fixed top-9 w-full p-2 lg:px-5 lg:py-3 z-50 flex items-center justify-between transition-all duration-300 bg-white ring-1 ring-gray-200"
   >
-    <UButton
-      class="text-black hover:text-zinc-700 block lg:hidden"
-      icon="iconamoon:menu-burger-horizontal-bold"
-      @click="isOpen = !isOpen"
-      variant="link"
-      size="xl"
-    />
     <Logo />
+    <UHorizontalNavigation
+      :links="links"
+      class="hidden xl:block w-max absolute left-1/2 transform -translate-x-1/2"
+      :ui="{
+        inactive: 'text-black hover:text-zinc-700',
+        active: 'text-orange-500 after:hidden',
+        before: '',
+      }"
+    />
     <section class="lg:mr-3 lg:ml-auto flex items-center">
-      <UHorizontalNavigation
-        :links="links"
-        class="hidden lg:block"
-        :ui="{
-          inactive: 'text-black hover:text-zinc-700',
-          active: 'text-orange-500 after:hidden',
-          before: '',
-        }"
-      />
       <UButton
         icon="material-symbols:lock-outline"
         v-if="!isAuthenticated"
@@ -100,6 +93,13 @@ const isOpen = ref(false);
         size="xl"
         @click="searchIsOpen = true"
       />
+      <UButton
+        class="text-black hover:text-zinc-700 block lg:hidden"
+        icon="iconamoon:menu-burger-horizontal-bold"
+        @click="isOpen = !isOpen"
+        variant="link"
+        size="xl"
+      />
     </section>
     <UButton
       class="rounded-full text-black hidden lg:block"
@@ -107,10 +107,10 @@ const isOpen = ref(false);
       to="/online-form"
       size="lg"
     />
-    <USlideover v-model="isOpen" side="left">
+    <USlideover v-model="isOpen">
       <section class="p-4 flex-1">
         <UButton
-          class="text-black ml-auto block mb-3"
+          class="text-black mb-3"
           icon="iconamoon:close"
           @click="isOpen = false"
           variant="link"
