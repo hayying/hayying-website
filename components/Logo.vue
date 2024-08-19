09ac627706fdeda734c4eacd49c906a4d5bc1732
hayying <hayyingdanismanlik2@gmail.com>
2024-08-19 13:56:25 +0300

    Update Logo.vue

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
   <NuxtLink to="/" class="flex-shrink-0">
-    <img src="/Logo_2.jpeg" alt="logo" class="h-12" />
+    <img src="/logo.png" alt="logo" class="h-12" />
   </NuxtLink>
 </template>
